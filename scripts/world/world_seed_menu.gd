b16bcc786ccc65a3745b2378c3db7d939496d38c
jasonjllind-cpu <jason.jl.lind@gmail.com>
2026-08-09 10:00:42 +0200

    Fix random seed generation

--- scripts/world/world_seed_menu.gd
+++ scripts/world/world_seed_menu.gd
@@ -109,9 +109,12 @@ func _on_seed_submitted(_value: String) -> void:
 func _randomize_seed() -> void:
 	var rng := RandomNumberGenerator.new()
 	rng.randomize()
-	seed_edit.text = str(rng.randi_range(100000, 2147483000))
-	seed_edit.grab_focus()
-	seed_edit.select_all()
+	var current_seed: int = _current_seed()
+	var seed_value: int = current_seed
+	while seed_value == current_seed:
+		seed_value = rng.randi_range(100000, 2147483000)
+	seed_edit.text = str(seed_value)
+	_apply_seed()
 
 func _apply_seed() -> void:
 	var text_value: String = seed_edit.text.strip_edges()
